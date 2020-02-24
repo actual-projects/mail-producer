8dd6dcfd93a654b082f842a767cf9ca544055bb7
zehua.wei <1450112292@qq.com>
2020-02-24 15:53:40 +0800

    mail-producer

---
+++ src/test/java/com/oxford/mail/MailProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oxford.mail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
