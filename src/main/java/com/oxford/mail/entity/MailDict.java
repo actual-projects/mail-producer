b338c72a3cdcf43fabed4765e85916eda009c8de
chova <chovavea@outlook.com>
2021-01-10 19:13:30 +0800

    添加tk.mybatis代码生成器

---
+++ src/main/java/com/oxford/mail/entity/MailDict.java
@@ -0,0 +1,127 @@
+package com.oxford.mail.entity;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+/**
+ * Mail字典 实体类
+ *
+ * @author Chova
+ * @date 2021/01/10
+ */
+@Table(name = "mail_dict")
+public class MailDict implements Serializable {
+    /**
+     * ID
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    /**
+     * 编码
+     */
+    private String code;
+
+    /**
+     * 名称
+     */
+    private String name;
+
+    /**
+     * 状态
+     */
+    private String status;
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 获取ID
+     *
+     * @return id - ID
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * 设置ID
+     *
+     * @param id ID
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * 获取编码
+     *
+     * @return code - 编码
+     */
+    public String getCode() {
+        return code;
+    }
+
+    /**
+     * 设置编码
+     *
+     * @param code 编码
+     */
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    /**
+     * 获取名称
+     *
+     * @return name - 名称
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * 设置名称
+     *
+     * @param name 名称
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * 获取状态
+     *
+     * @return status - 状态
+     */
+    public String getStatus() {
+        return status;
+    }
+
+    /**
+     * 设置状态
+     *
+     * @param status 状态
+     */
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", code=").append(code);
+        sb.append(", name=").append(name);
+        sb.append(", status=").append(status);
+        sb.append(", serialVersionUID=").append(serialVersionUID);
+        sb.append("]");
+        return sb.toString();
+    }
+}
---
+++ src/main/java/com/oxford/mail/mapper/MailDictMapper.java
@@ -0,0 +1,13 @@
+package com.oxford.mail.mapper;
+
+import com.oxford.mail.entity.MailDict;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * Mail字典 Mapper接口
+ *
+ * @author Chova
+ * @date 2021/01/10
+ */
+public interface MailDictMapper extends Mapper<MailDict> {
+}
